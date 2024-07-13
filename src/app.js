16e00f6cf4ea6a932256f99caf249fc783ca5da2
Ramanand Kumar <rk1074501@gmail.com>
2024-07-13 11:08:51 +0530

    add utility functions in project

--- src/app.js
+++ src/app.js
@@ -1,2 +1,28 @@
 import express from "express";
-import nodemon from "nodemon";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json(
+    {limit:"20kb"}
+))
+
+app.use(express.urlencoded({
+    limit:"20kb",
+    extended:true,
+}))
+app.use(express.static("public"))
+
+
+
+app.use(cookieParser())
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config()
-console.log(process.env)
+dotenv.config();
+import {app} from "./app.js"
+
 
 import connectDB from "./config/database.js";
 
 
 connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000,
+        ()=>{
+           console.log( `server is running at port no. : ${process.env.PORT}`)
+        }
+    )
+})
+.catch((err)=>{
+    console.error("error in server starting ", err)
+})
