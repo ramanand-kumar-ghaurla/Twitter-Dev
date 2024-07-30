8fff6e80809844a9519cdaa689fd40d9e53d8eb4
Learn Web <learnweb005@gmail.com>
2024-07-30 17:46:01 +0530

    user login feature implemented

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,78 +2,172 @@ import {asyncHandler} from "../utiles/asyncHandler.js"
 import{apiError} from "../utiles/ApiError.js"
 import {User} from "../models/user.model.js"
 import {apiResponse} from "../utiles/apiResponse.js"
+import bcrypt from "bcrypt";
 
+
+// function for generating referesh and access token
+
+const generateAccessAndRefereshToken= async (userId)=>{
+    try {
+
+        const user = await User.findById(userId)
+        const accessToken= user.generateAccessToken()
+        const refereshToken= user.generateRefereshToken()
+
+        user.refereshToken = refereshToken
+        user.save({validateBeforeSave:false})
+
+        return {accessToken,refereshToken}
+        
+    } catch (error) {
+        throw new apiError(500,
+            "error in generating referesh and access token"
+        )
+        
+    }
+}
 
 
 const registerUser = asyncHandler( async (req,res)=>{
 
-    // find the details from req
-
-    const { username,email,fullName,password } = req.body
-    
-
-    // validation on the all fields
-
-    if(!username && !email && !fullName && !password){
-        new apiError(
-                    400,
-                    "all fields are required",
-        )
-
-    }
-
-    // if (
-    //     [username,email,fullName,password].some((field)=>{
-    //         field === ""
-    //     })
-    // ){
-    //     new apiError(
-    //         400,
-    //         "all fields are required",
-
-    //     )
-    // }
-
-
- const existedUser = await User.findOne({
-    $or:[{username},{password}]
-  
-})
-
-
- if(existedUser){
-    throw new apiError(400, "user with this email and username  is already exists")
+   try {
+     // find the details from req
+ 
+     const { username,email,fullName,password } = req.body
+     
+ 
+     // validation on the all fields
+ 
+     if(!username && !email && !fullName && !password){
+         new apiError(
+                     400,
+                     "all fields are required",
+         )
+ 
+     }
+ 
+     // if (
+     //     [username,email,fullName,password].some((field)=>{
+     //         field === ""
+     //     })
+     // ){
+     //     new apiError(
+     //         400,
+     //         "all fields are required",
+ 
+     //     )
+     // }
+ 
+ 
+  const existedUser = await User.findOne({
+     $or:[{username},{password}]
+   
+ })
+ 
+ 
+  if(existedUser){
+     throw new apiError(400, "user with this email and username  is already exists")
+  }
+ 
+ //  create the user 
+ 
+  const user = await User.create({
+     username,
+     email,
+     fullName,
+     password
+ })
+ 
+ // check if the user is created or not
+ 
+ const createdUser = await User.findById(user._id).select(
+     "-password -refereshToken"
+ )
+ 
+ if (!createdUser){
+     throw new apiError(500, "error in registring user")
  }
-
-//  create the user 
-
- const user = await User.create({
-    username,
-    email,
-    fullName,
-    password
+ 
+ return res.status(201).json(
+     new apiResponse(
+         200,
+         createdUser,
+         "User created successfully",
+     )
+ )
+ 
+ 
+ 
+   } catch (error) {
+    throw new apiError(
+        500,
+        "error in registring user",
+        console.log(error)
+    )
+    
+   }
 })
 
-// check if the user is created or not
 
-const createdUser = await User.findById(user._id).select(
-    "-password -refereshToken"
-)
 
-if (!createdUser){
-    throw new apiError(500, "error in registring user")
+// method for login user
+
+const loginUser = asyncHandler(async(req,res)=>{
+
+// fetch data from reqest body
+const {username,email,password} = req.body
+
+// validation
+
+if(!(username || email)){
+    throw new apiError(400,"username or email is requires")
+
 }
 
-return res.status(201).json(
+ const user = await User.findOne({
+    $or:[{username},{email}]
+ })
+
+ if(!user){
+    throw new apiError(400,"User does not exists")
+
+
+}
+// check the password
+
+const isPasswordValid = await user.isPasswordCorrect(password)
+
+if(!isPasswordValid){
+    throw new apiError(400,"Password is incorrect")
+
+}
+
+// generate access and referesh token to te user
+
+ const {accessToken,refereshToken}=await generateAccessAndRefereshToken(user._id)
+
+ const loggedInUser = await User.findById(user._id).select("-password -refereshToken")
+
+
+ const options={
+    httpOnly:true,
+    secure:true
+
+ }
+ return res.status(200)
+ .cookie("accessToken",accessToken,options)
+ .cookie("refereshToken",refereshToken,options)
+ .json(
     new apiResponse(
         200,
-        createdUser,
-        "User created successfully",
+        {
+            user:loggedInUser,refereshToken,accessToken
+        },
+        "user logged in successfully"
     )
-)
-
-
+ )
 
 })
 
-export {registerUser}
+
+export {registerUser ,loginUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { Router } from "express";
-import {registerUser} from '../controllers/user.controller.js';
+import {loginUser, registerUser} from '../controllers/user.controller.js';
 import bodyParser from "body-parser";
 
 
 const router = Router()
-
-router.route("/register").post(registerUser);
 router.use(bodyParser.json());
+
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+
+
+
 
 
 
